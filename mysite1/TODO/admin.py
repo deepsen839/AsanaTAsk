a87364ab0af64e9e839c4926c594f350cc668b5b
deepsen839 <79280023+deepsen839@users.noreply.github.com>
2022-02-11 20:36:04 +0530

    Delete mysite1 directory

--- mysite1/TODO/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import asanaModel
-# Register your models here.
-@admin.register(asanaModel)
-class asanaModelAdmin(admin.ModelAdmin):
-    #fields = ('taskname','notes')
-    list_display = ('taskname','notes')
